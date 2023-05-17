6c4811766ad3b69b6226b23ef0a5b13cf8c89ec3
trynde <tjoao9906@gmail.com>
2023-05-16 21:37:23 -0300

    loja

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Produto.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Carolina
+ */
+// Produto.java
+public class Produto {
+    private String nome;
+    private String descricao;
+    private double preco;
+    
+    public Produto() {
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Produto(String nome, String descricao, double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    
+}
